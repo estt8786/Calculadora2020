865a2e418633d9fb2533b0f4b0ec40e6e4654dd9
Virgilio Quintino <estt8786@ipt.pt>
2020-03-16 22:18:19 +0000

    Código quase final da calculadora. Falta melhorar CSS e resolver o problema do apagar "c"

--- Calculadora/Controllers/HomeController.cs
+++ Calculadora/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ namespace Calculadora.Controllers
 {
     public class HomeController : Controller
     {
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -21,16 +22,116 @@ namespace Calculadora.Controllers
         /// <summary>
         /// Apresenta a view calculadora no primeiro pedido - HTTP GET
         /// </summary>
+        /// <param name="bt">identifica o valor da tecla clicada</param>
         /// <returns></returns>
 
         public IActionResult Index()
         {
+            // inicializar o valor do visor
+            ViewBag.Visor = "0";
+            ViewBag.LimparVisor = "true";
+
             return View();
         }
 
+
         [HttpPost]
-        public IActionResult Index(string a)
+        public IActionResult Index(string visor, string bt, string operando, string operador, bool limparVisor)
         {
+            // identificar o valor da variável "bt"
+            switch (bt)
+            {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                case "0":
+                    if (visor == "0" || limparVisor) visor = bt;
+                    else visor += bt; //visor = visor + bt;
+
+                    // impedir o visor de ser limpo
+                    limparVisor = false;
+
+                    break;
+
+                case "+/-":
+                    // inverter o valor do visor
+                    // pode ser feito de duas formas:
+                    //  - multiplicar por -1 -> converter o valor para numero
+                    //  - processar a string: visor.StartsWith().ToString().Substring().Length
+                    visor = Convert.ToDouble(visor) * -1 + "";
+
+                    break;
+
+                case ",":
+                    if (!visor.Contains(",")) visor += bt;
+                    break;
+
+                case "+":
+                case "-":
+                case ":":
+                case "x":
+                case "=":
+
+                    //primeira vez que um operador foi selecionado
+                    if (operador != null)
+                    {
+
+                        // executar a operação
+                        // variaveis auxiliares
+                        double operando1 = Convert.ToDouble(operando);
+                        double operando2 = Convert.ToDouble(visor);
+
+                        switch (operador)
+                        {
+                            case "+":
+                                visor = operando1 + operando2 + "";
+                                break;
+                            case "-":
+                                visor = operando1 - operando2 + "";
+                                break;
+                            case "x":
+                                visor = operando1 * operando2 + "";
+                                break;
+                            case ":":
+                                visor = operando1 / operando2 + "";
+                                break;
+
+                            case "c":
+                                visor = "0";
+                                operador = "";
+                                operando = "";
+                                limparVisor = true;
+                                break;
+
+                        }
+ 
+
+            }
+                    // guardar valores para "memoria futura"
+                    if (bt != "=") operador = bt;
+                    else operador = "";
+                    operando = visor;
+                    limparVisor = true;
+                    // guardar valores para "memoria futura"
+                    //operador = bt;
+                    break;
+
+
+
+            }
+
+            // levar o resultado do visor
+            ViewBag.Visor = visor;
+            ViewBag.Operador = operador;
+            ViewBag.Operando = operando;
+            ViewBag.LimparVisor = limparVisor + "";
+
             return View();
         }
 
